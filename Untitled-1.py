099d2bdb123d4dd7f38e20ef7e3c8d92343e7405
Mouaadyacine <146827267+Mouaadyacine@users.noreply.github.com>
2023-10-08 14:13:12 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+a=int( input("svp entrer un age superieur a 6") )
+if a==6 or a==7:
+    print("poussin")
+elif a==8 or a==9:
+    print("pupille")
+elif a==10 or a==11:
+    print("minime")
+elif a>12 or a==12:
+    print("cadet")
+else:
+    print("erreur")
